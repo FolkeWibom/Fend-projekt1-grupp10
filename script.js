c150bf6aae46ae2aa2b91369cedfa73fd7865dc1
karber <karl.berglof@hotmail.com>
2019-02-06 11:18:52 +0100

    bugfix

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function dropDownFunc() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function dropDownFuncHtml() {
+
+  document.getElementById("myDropdownHTML").classList.toggle("show");
+  }
+  function dropDownfuncCSS(){
+    document.getElementById("myDropdownCSS").classList.toggle("show");
+  }
+  function dropDownfuncJS(){
+    document.getElementById("myDropdownJS").classList.toggle("show");
+  }
+  function dropDownfuncPS(){
+    document.getElementById("myDropdownPS").classList.toggle("show");
   }
   
   // Close the dropdown if the user clicks outside of it
