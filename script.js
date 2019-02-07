b4816246b750eb628d53e1c723cdeb2df03481b4
karber <karl.berglof@hotmail.com>
2019-02-07 11:52:22 +0100

    headerfix

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ function dropDownFuncHtml() {
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       let dropdowns = document.getElementsByClassName("dropdown-content");
-      let i;
-      for (i = 0; i < dropdowns.length; i++) {
+      
+      for (let i = 0; i < dropdowns.length; i++) {
         let openDropdown = dropdowns[i];
         if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
